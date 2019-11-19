5b9fd4fe65acbd68a2752bcd37203c4c1f1415a0
Eric Morris <git@thedeerchild.com>
2019-11-18 22:19:01 -0800

    add(e2e): split into helper files and make more robust

--- api/e2e/test-client.go
+++ api/e2e/test-client.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"bufio"
-	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
+	"reflect"
+	"runtime"
 	"strings"
-	"time"
 
 	"google.golang.org/grpc"
 
@@ -20,91 +18,15 @@ const (
 	address = "localhost:50051"
 )
 
-func logResponse(resp interface{}, err error) {
-	if err != nil {
-		log.Printf("Error making call: %v", err)
-	}
-
-	if respStr, err := json.MarshalIndent(resp, "", "\t"); err != nil {
-		log.Printf("Error formatting response as JSON: %v", err)
-	} else {
-		log.Printf("Response:\n%s", respStr)
-	}
-}
-
-func GetSchedule(client pg.APIClient) {
-	log.Println("Making call to GetSchedule")
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	r, err := client.GetSchedule(ctx, &pg.GetScheduleRequest{
-		EventKey: "nyc-2019",
-	})
-	logResponse(r, err)
-}
-
-func RegisterPlayer(client pg.APIClient) {
-	log.Println("Making call to RegisterPlayer")
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	r, err := client.RegisterPlayer(ctx, &pg.RegisterPlayerRequest{
-		EventKey:    "nyc-2019",
-		PhoneNumber: "+5551231234",
-		Name:        "Eric Morris",
-		// League:      pg.League_LPGA,
-	})
-	logResponse(r, err)
-}
-
-func RequestPlayerLogin(client pg.APIClient) {
-	log.Println("Making call to RequestPlayerLogin")
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	r, err := client.RequestPlayerLogin(ctx, &pg.RequestPlayerLoginRequest{
-		EventKey:    "nyc-2019",
-		PhoneNumber: "+5551231234",
-	})
-	logResponse(r, err)
-}
-
-func PlayerLogin(client pg.APIClient) {
-	buf := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter login code:")
-	fmt.Print("> ")
-	code, err := buf.ReadBytes('\n')
-	if err != nil {
-		log.Printf("Could not read input: %s", err)
-	}
-
-	codeAsNum, err := strconv.Atoi(string(code[:len(code)-1]))
-	if err != nil {
-		log.Printf("Could not parse input as a number: %s", err)
-	}
-
-	log.Println("Making call to PlayerLogin")
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	r, err := client.PlayerLogin(ctx, &pg.PlayerLoginRequest{
-		EventKey:    "nyc-2019",
-		PhoneNumber: "+5551231234",
-		AuthCode:    uint32(codeAsNum),
-	})
-	logResponse(r, err)
-}
-
-func get(r *bufio.Reader) string {
-	t, _ := r.ReadString('\n')
-	return strings.TrimSpace(t)
-}
-
 func printHelp() {
 	fmt.Println("")
 	fmt.Println("Enter number to select RPC method to test")
 	fmt.Println("(or type 'q' to quit)")
 	fmt.Println("-----------------------------------------")
-	fmt.Println("1: GetSchedule")
-	fmt.Println("2: RegisterPlayer")
-	fmt.Println("3: RequestPlayerLogin")
-	fmt.Println("4: PlayerLogin")
+	for label, fn := range funcMap {
+		fmt.Printf("%s: %s\n", label,
+			strings.Split(runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name(), ".")[1])
+	}
 	fmt.Print("> ")
 }
 
@@ -120,18 +42,11 @@ func main() {
 
 	buf := bufio.NewReader(os.Stdin)
 	printHelp()
-	cmd := get(buf)
-	for ; cmd != "q"; cmd = get(buf) {
+	cmd := getCmd(buf)
+	for ; cmd != "q"; cmd = getCmd(buf) {
 		fmt.Println("")
-		switch cmd {
-		case "1":
-			GetSchedule(client)
-		case "2":
-			RegisterPlayer(client)
-		case "3":
-			RequestPlayerLogin(client)
-		case "4":
-			PlayerLogin(client)
+		if fn, exists := funcMap[cmd]; exists {
+			fn(client)
 		}
 		printHelp()
 	}
