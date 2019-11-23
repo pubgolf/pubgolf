85ef7c010d42163ce466fa13e9cd56699ebf8fa4
Alexander <Alex-CS@users.noreply.github.com>
2019-11-23 00:54:44 -0500

    Get the correct API hostnames for environment

--- web-app/src/api.js
+++ web-app/src/api.js
@@ -42,13 +42,25 @@ export const getCookieJar = () => {
   };
 };
 
+function getAPIHostname () {
+  const hostNames = {
+    dev: 'http://127.0.0.1:8080',
+    prod: 'https://api.pubgolf.co',
+    staging: 'https://api-staging.pubgolf.co',
+  };
+  if (process.env.PUBGOLF_ENV) {
+    return hostNames[process.env.PUBGOLF_ENV];
+  }
+  return hostNames.dev;
+}
+
 const TOKEN_COOKIE = 'pg-token';
 
 class API {
   constructor (eventKey, metadata = {}) {
     this._cookieJar = getCookieJar();
     this.eventKey = eventKey;
-    this.client = new APIPromiseClient('http://127.0.0.1:8080');
+    this.client = new APIPromiseClient(getAPIHostname());
     this.metadata = metadata;
 
     if (!metadata.authorization) {
