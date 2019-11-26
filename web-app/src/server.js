0586c73c451d9416703efcf0f3db49e9096aadaf
Eric Morris <git@thedeerchild.com>
2019-11-26 01:43:54 -0500

    remove(server): static asset server

--- web-app/src/server.js
+++ web-app/src/server.js
@@ -1,4 +1,3 @@
-import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
@@ -10,7 +9,6 @@ const dev = NODE_ENV === 'development';
 polka() // You can also use Express
   .use(
     compression({ threshold: 0 }),
-    sirv('static', { dev }),
     sapper.middleware(),
   ).listen(PORT, err => {
   if (err) console.log('error', err);
