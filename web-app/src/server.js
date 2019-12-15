ba0d171da851864e744e42f429d6998febe82341
Eric Morris <git@thedeerchild.com>
2019-12-14 18:00:43 -0800

    add(web-app/server): session config from env vars

--- web-app/src/server.js
+++ web-app/src/server.js
@@ -1,15 +1,25 @@
-import polka from 'polka';
-import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+import compression from 'compression';
+import dotenv from 'dotenv';
+import polka from 'polka';
 
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+// Attempt to load .env from the monorepo root, since the dev server gets run
+// from the web-app directory. If this file doesn't exist, we're running inside
+// Docker and have actual env vars already loaded.
+dotenv.config({path: '../.env'})
 
-polka() // You can also use Express
+polka()
   .use(
     compression({ threshold: 0 }),
-    sapper.middleware(),
-  ).listen(PORT, err => {
+    sapper.middleware({
+      session: (req, res) => ({
+        config: {
+          PUBGOLF_ENV: process.env.PUBGOLF_ENV,
+          API_HOST_EXTERNAL: process.env.$API_HOST_EXTERNAL,
+        }
+      }),
+    }),
+).listen(process.env.PORT, err => {
   if (err) console.log('error', err);
 });
