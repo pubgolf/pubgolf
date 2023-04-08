08def211e45c93a47d9b1cd301b5f933a092c04b
Eric Morris <accounts+github@thedeerchild.com>
2023-04-07 22:05:16 -0400

    add(rpc): dao dep injection

--- api/internal/lib/rpc/rpc.go
+++ api/internal/lib/rpc/rpc.go
@@ -1,10 +1,21 @@
 package rpc
 
 import (
-	"github.com/pubgolf/pubgolf/api/internal/gen/proto/api/v1/apiv1connect"
+	"database/sql"
+
+	"github.com/pubgolf/pubgolf/api/internal/lib/dao"
+	"github.com/pubgolf/pubgolf/api/internal/lib/proto/api/v1/apiv1connect"
 )
 
 // PubGolfServiceServer implements the gRPC handlers for the PubGolf API.
 type PubGolfServiceServer struct {
 	apiv1connect.UnimplementedPubGolfServiceHandler
+	dao dao.QueryProvider
+}
+
+// NewPubGolfServiceServer constructs a gRPC server implementation with data access dependencies injected.
+func NewPubGolfServiceServer(db *sql.DB) *PubGolfServiceServer {
+	return &PubGolfServiceServer{
+		dao: dao.New(db),
+	}
 }
