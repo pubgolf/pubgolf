94ca44f07d6daab9c3b9707b316d093ae7e2bf2f
Eric Morris <accounts+github@thedeerchild.com>
2023-04-14 03:16:49 -0400

    add(roc): create and read player admin api methods

---
+++ api/internal/lib/rpc/admin/create_player.go
@@ -0,0 +1,41 @@
+package admin
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/bufbuild/connect-go"
+
+	"github.com/pubgolf/pubgolf/api/internal/lib/models"
+	apiv1 "github.com/pubgolf/pubgolf/api/internal/lib/proto/api/v1"
+	"github.com/pubgolf/pubgolf/api/internal/lib/telemetry"
+)
+
+// CreatePlayer registers a player in the given event, returning the player's ID. This method is idempotent, so if the player is already registered the request will still succeed.
+func (s *Server) CreatePlayer(ctx context.Context, req *connect.Request[apiv1.CreatePlayerRequest]) (*connect.Response[apiv1.CreatePlayerResponse], error) {
+	telemetry.AddRecursiveAttribute(&ctx, "event.key", req.Msg.EventKey)
+
+	eventID, err := s.dao.EventIDByKey(ctx, req.Msg.EventKey)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, connect.NewError(connect.CodeNotFound, err)
+		}
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	player := models.PlayerParams{Name: req.Msg.Player.Name}
+	err = player.ScoringCategory.FromProtoEnum(req.Msg.Player.ScoringCategory)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+
+	playerID, err := s.dao.UpsertPlayer(ctx, eventID, player)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	return connect.NewResponse(&apiv1.CreatePlayerResponse{
+		PlayerId: playerID.String(),
+	}), nil
+}
---
+++ api/internal/lib/rpc/admin/list_players.go
@@ -0,0 +1,50 @@
+package admin
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/bufbuild/connect-go"
+
+	apiv1 "github.com/pubgolf/pubgolf/api/internal/lib/proto/api/v1"
+	"github.com/pubgolf/pubgolf/api/internal/lib/telemetry"
+)
+
+// ListPlayers returns a list of players registered for the given event in alphabetical order by name.
+func (s *Server) ListPlayers(ctx context.Context, req *connect.Request[apiv1.ListPlayersRequest]) (*connect.Response[apiv1.ListPlayersResponse], error) {
+	telemetry.AddRecursiveAttribute(&ctx, "event.key", req.Msg.EventKey)
+
+	eventID, err := s.dao.EventIDByKey(ctx, req.Msg.EventKey)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, connect.NewError(connect.CodeNotFound, err)
+		}
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	dbPlayers, err := s.dao.EventPlayers(ctx, eventID)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	players := make([]*apiv1.ListPlayersResponse_PlayerInfo, 0, len(dbPlayers))
+	for _, p := range dbPlayers {
+		cat, err := p.ScoringCategory.ProtoEnum()
+		if err != nil {
+			return nil, connect.NewError(connect.CodeUnknown, err)
+		}
+
+		player := apiv1.ListPlayersResponse_PlayerInfo{
+			Id:              p.ID.String(),
+			Name:            p.Name,
+			ScoringCategory: cat,
+		}
+
+		players = append(players, &player)
+	}
+
+	return connect.NewResponse(&apiv1.ListPlayersResponse{
+		Players: players,
+	}), nil
+}
