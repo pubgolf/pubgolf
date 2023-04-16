9cc61c1e94490f06dd4cbd1bbbb9d6789bf8cd9b
Eric Morris <accounts+github@thedeerchild.com>
2023-04-16 19:55:20 -0400

    add(telemetry): record gRPC args and errors

--- api/internal/lib/middleware/logger.go
+++ api/internal/lib/middleware/logger.go
@@ -9,8 +9,11 @@ import (
 	"time"
 
 	"github.com/bufbuild/connect-go"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
+// NewLoggingInterceptor logs (and annotates OTel spans for) all gRPC calls.
 func NewLoggingInterceptor() connect.UnaryInterceptorFunc {
 	interceptor := func(next connect.UnaryFunc) connect.UnaryFunc {
 		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
@@ -23,7 +26,12 @@ func NewLoggingInterceptor() connect.UnaryInterceptorFunc {
 			method := parts[2]
 			args, _ := json.Marshal(req.Any())
 
+			span := trace.SpanFromContext(ctx)
+			span.SetAttributes(attribute.String("rpc.args", string(args)))
+
 			if connectErr := new(connect.Error); errors.As(err, &connectErr) {
+				span.SetAttributes(attribute.String("error.message", connectErr.Message()))
+
 				log.Printf("%s.%s(%s) completed in %q with code: \"%d %s\" error: %q\n", service, method, args, duration, connectErr.Code(), connectErr.Code(), connectErr.Message())
 			} else {
 				log.Printf("%s.%s(%s) completed in %q successfully\n", service, method, args, duration)
