3caabdd3f21be5ab600418effcec1eabc41dfc6b
Eric Morris <accounts+github@thedeerchild.com>
2023-04-17 19:58:54 -0400

    fix(webapp/config): fix postcss support

--- web-app/svelte.config.js
+++ web-app/svelte.config.js
@@ -1,22 +1,35 @@
+import { dirname, join } from 'path';
+
 import adapter from '@sveltejs/adapter-static';
+import { fileURLToPath } from 'url';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess()],
+	preprocess: [
+		vitePreprocess({
+			style: {
+				css: {
+					postcss: join(__dirname, 'postcss.config.cjs')
+				}
+			},
+		})
+	],
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
 		}),
-        paths: {
-            assets: 'https://assets.pubgolf.co',
-        },
+		paths: {
+			assets: 'https://assets.pubgolf.co'
+		},
 		prerender: {
 			entries: [
 				'/',
 				'/event/nyc-2023',
-                '/about/contact',
-                '/about/privacy',
+				'/about/contact',
+				'/about/privacy',
 				'/admin',
 				'/admin/nyc-2023',
 				'/admin/nyc-2023/alerts',
