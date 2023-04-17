393fa0c6683bbfa5a3056fb83ac3e94428b5b3d3
Eric Morris <accounts+github@thedeerchild.com>
2023-04-16 20:09:34 -0400

    chore(proto): move score category to shared file

--- web-app/src/lib/proto/api/v1/admin_pb.ts
+++ web-app/src/lib/proto/api/v1/admin_pb.ts
@@ -7,38 +7,7 @@
 
 import type { BinaryReadOptions, FieldList, JsonReadOptions, JsonValue, PartialMessage, PlainMessage } from "@bufbuild/protobuf";
 import { Message, proto3 } from "@bufbuild/protobuf";
-
-/**
- * @generated from enum api.v1.ScoringCategory
- */
-export enum ScoringCategory {
-  /**
-   * @generated from enum value: SCORING_CATEGORY_UNSPECIFIED = 0;
-   */
-  UNSPECIFIED = 0,
-
-  /**
-   * @generated from enum value: SCORING_CATEGORY_PUB_GOLF_NINE_HOLE = 1;
-   */
-  PUB_GOLF_NINE_HOLE = 1,
-
-  /**
-   * @generated from enum value: SCORING_CATEGORY_PUB_GOLF_FIVE_HOLE = 2;
-   */
-  PUB_GOLF_FIVE_HOLE = 2,
-
-  /**
-   * @generated from enum value: SCORING_CATEGORY_PUB_GOLF_CHALLENGES = 3;
-   */
-  PUB_GOLF_CHALLENGES = 3,
-}
-// Retrieve enum metadata with: proto3.getEnumType(ScoringCategory)
-proto3.util.setEnumType(ScoringCategory, "api.v1.ScoringCategory", [
-  { no: 0, name: "SCORING_CATEGORY_UNSPECIFIED" },
-  { no: 1, name: "SCORING_CATEGORY_PUB_GOLF_NINE_HOLE" },
-  { no: 2, name: "SCORING_CATEGORY_PUB_GOLF_FIVE_HOLE" },
-  { no: 3, name: "SCORING_CATEGORY_PUB_GOLF_CHALLENGES" },
-]);
+import { ScoringCategory } from "./shared_pb.js";
 
 /**
  * @generated from message api.v1.CreatePlayerRequest
