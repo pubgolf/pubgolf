c6078b3a7b792de6e0e34e8e15a9ff2c3f53e030
Eric Morris <accounts+github@thedeerchild.com>
2023-04-16 22:55:04 -0400

    add(webapp): landing page for NYC 2023

---
+++ web-app/src/routes/event/[eventKey]/+page.svelte
@@ -0,0 +1,18 @@
+<div class="container mx-auto h-screen p-4 flex flex-col justify-center items-center">
+	<div class="text-center">
+		<h1 class="mb-2">NYC Bottle Open ’23</h1>
+		<p class="h3 leading-10">Saturday, April 22 2023</p>
+		<p>
+			More details on <a href="https://partiful.com/e/lmnAAHczXC3hXQop6lmS">Partiful</a>.
+		</p>
+	</div>
+</div>
+<footer class="fixed text-center w-full bottom-4">
+	<p>With <span class="bg-primary-500 py-0.5 px-1 rounded">&#60;3</span> from Kaitlyn & Eric</p>
+</footer>
+
+<style lang="postcss">
+	/* .heart {
+		@apply text-primary-500;
+	} */
+</style>
