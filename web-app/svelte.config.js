d824a381db1af8209fe91c924fc17fed73ad818a
Eric Morris <accounts+github@thedeerchild.com>
2023-04-17 00:05:46 -0400

    add(webapp): deploy pipeline

--- web-app/svelte.config.js
+++ web-app/svelte.config.js
@@ -1,16 +1,28 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-	],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+        paths: {
+            assets: 'https://assets.pubgolf.co',
+        },
+		prerender: {
+			entries: [
+				'/',
+				'/event/nyc-2023',
+				'/admin',
+				'/admin/nyc-2023',
+				'/admin/nyc-2023/alerts',
+				'/admin/nyc-2023/players',
+				'/admin/nyc-2023/schedule',
+				'/admin/nyc-2023/scores'
+			]
+		}
 	}
 };
 
