eeccaf0e00e10c4c8d6a8acd5d77a6b363e589ff
Eric Morris <accounts+github@thedeerchild.com>
2023-04-19 04:14:09 -0400

    add(webapi): error and parsing helpers

---
+++ api/internal/lib/webapi/error.go
@@ -0,0 +1,60 @@
+package webapi
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/render"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type errorCode string
+
+const (
+	errorCodeGenericRetryable    errorCode = "ERR_GENERIC_SERVER_RETRYABLE"
+	errorCodeGenericNonRetryable errorCode = "ERR_GENERIC_SERVER_NON_RETRYABLE"
+	errorCodeMalformedRequest    errorCode = "ERR_MALFORMED_REQUEST"
+	errorCodeNotAuthorized       errorCode = "ERR_NOT_AUTHORIZED"
+)
+
+var errorCodeHTTPStatuses = map[errorCode]int{
+	errorCodeGenericRetryable:    http.StatusInternalServerError,
+	errorCodeGenericNonRetryable: http.StatusInternalServerError,
+	errorCodeMalformedRequest:    http.StatusBadRequest,
+	errorCodeNotAuthorized:       http.StatusUnauthorized,
+}
+
+func allErrorCodes() []errorCode {
+	all := make([]errorCode, len(errorCodeHTTPStatuses))
+	for e := range errorCodeHTTPStatuses {
+		all = append(all, e)
+	}
+	return all
+}
+
+func (e errorCode) HTTPStatus() int {
+	return errorCodeHTTPStatuses[e]
+}
+
+type errorResponse struct {
+	PrivateErr error     `json:"-"`
+	RootSpanID string    `json:"root_span_id"`
+	Code       errorCode `json:"error_code"`
+	Msg        string    `json:"error_message"`
+}
+
+func newErrorResponse(ctx context.Context, code errorCode, msg string, err error) errorResponse {
+	return errorResponse{
+		PrivateErr: err,
+		RootSpanID: trace.SpanFromContext(ctx).SpanContext().TraceID().String(),
+		Code:       code,
+		Msg:        msg,
+	}
+}
+
+func (e errorResponse) Render(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handler error: %+v\n", e.PrivateErr)
+	render.Status(r, e.Code.HTTPStatus())
+	render.JSON(w, r, e)
+}
---
+++ api/internal/lib/webapi/request.go
@@ -0,0 +1,91 @@
+package webapi
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type malformedRequest struct {
+	OriginalErr error
+	Status      errorCode
+	Msg         string
+}
+
+func (mr *malformedRequest) Error() string {
+	return mr.Msg
+}
+
+func parseJSONRequest(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	if r.Header.Get("Content-Type") != "application/json" {
+		msg := "Content-Type header is not application/json"
+		return &malformedRequest{OriginalErr: errors.New("unexpected format error"), Status: errorCodeMalformedRequest, Msg: msg}
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(&dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+			return &malformedRequest{OriginalErr: err, Status: errorCodeMalformedRequest, Msg: msg}
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON")
+			return &malformedRequest{OriginalErr: err, Status: errorCodeMalformedRequest, Msg: msg}
+
+		case errors.As(err, &unmarshalTypeError):
+			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			return &malformedRequest{OriginalErr: err, Status: errorCodeMalformedRequest, Msg: msg}
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+			return &malformedRequest{OriginalErr: err, Status: errorCodeMalformedRequest, Msg: msg}
+
+		case errors.Is(err, io.EOF):
+			msg := "Request body must not be empty"
+			return &malformedRequest{OriginalErr: err, Status: errorCodeMalformedRequest, Msg: msg}
+
+		case err.Error() == "http: request body too large":
+			msg := "Request body must not be larger than 1MB"
+			return &malformedRequest{OriginalErr: err, Status: errorCodeMalformedRequest, Msg: msg}
+
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		msg := "Request body must only contain a single JSON object"
+		return &malformedRequest{OriginalErr: err, Status: errorCodeMalformedRequest, Msg: msg}
+	}
+
+	return nil
+}
+
+func guardParseJSONRequest(ctx context.Context, err error, w http.ResponseWriter, r *http.Request) (ok bool) {
+	if err == nil {
+		return true
+	}
+
+	var mr *malformedRequest
+	if errors.As(err, &mr) {
+		newErrorResponse(ctx, mr.Status, mr.Msg, fmt.Errorf("parse JSON request body: %w", mr.OriginalErr)).Render(w, r)
+	} else {
+		newErrorResponse(ctx, errorCodeGenericNonRetryable, "Unknown error parsing request body", fmt.Errorf("parse JSON request body: %w", err)).Render(w, r)
+	}
+	return false
+}
