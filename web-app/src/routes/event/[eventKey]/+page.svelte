e63e4f87c644c8aaa0c005f721350879aca4cfdd
Eric Morris <accounts+github@thedeerchild.com>
2023-04-21 05:01:35 -0400

    add(webapp): nav footer

--- web-app/src/routes/event/[eventKey]/+page.svelte
+++ web-app/src/routes/event/[eventKey]/+page.svelte
@@ -15,4 +15,9 @@
 </div>
 <footer class="fixed text-center w-full bottom-4">
 	<p>With <span class="bg-primary-500 py-0.5 px-1 rounded">&#60;3</span> from Kaitlyn & Eric</p>
+	<nav class="flex justify-center mt-4 gap-4 text-xs">
+		<a href="/admin">Admin</a>
+		<a href="/about/privacy">Privacy Policy</a>
+		<a href="/about/contact">Legal Contact</a>
+	</nav>
 </footer>
