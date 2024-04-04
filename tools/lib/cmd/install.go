84b77dbd8018182742dfbafd334a338d86ca849f
Eric Morris <git@thedeerchild.com>
2024-04-03 21:21:47 -0400

    add(cli/install): programmatically generate install commands

--- tools/lib/cmd/install.go
+++ tools/lib/cmd/install.go
@@ -8,13 +8,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var goTools = map[string]string{
+	"migrate":    "github.com/golang-migrate/migrate/v4/cmd/migrate@v4.17.0",
+	"sqlc":       "github.com/sqlc-dev/sqlc/cmd/sqlc@v1.24.0",
+	"mockery":    "github.com/vektra/mockery/v2@v2.38.0",
+	"ifacemaker": "github.com/vburenin/ifacemaker@v1.2.1",
+	"enumer":     "github.com/dmarkham/enumer@v1.5.9",
+}
+
 func init() {
 	installCmd.AddCommand(installDopplerCmd)
 	installCmd.AddCommand(installBufCmd)
-	installCmd.AddCommand(installMigrateCmd)
-	installCmd.AddCommand(installSQLcCmd)
-	installCmd.AddCommand(installMockeryCmd)
-	installCmd.AddCommand(installIfacemakerCmd)
+
+	for _, cmd := range generateGoInstallers() {
+		installCmd.AddCommand(cmd)
+	}
+
 	rootCmd.AddCommand(installCmd)
 }
 
@@ -25,20 +34,34 @@ var installCmd = &cobra.Command{
 		installers := []*cobra.Command{
 			installDopplerCmd,
 			installBufCmd,
-			installMigrateCmd,
-			installSQLcCmd,
-			installMockeryCmd,
-			installIfacemakerCmd,
 		}
+
+		installers = append(installers, generateGoInstallers()...)
 
 		runPar(cmd, args, installers...)
 	},
 }
 
+func generateGoInstallers() []*cobra.Command {
+	cs := make([]*cobra.Command, 0, len(goTools))
+
+	for n, v := range goTools {
+		cs = append(cs, &cobra.Command{
+			Use:   n,
+			Short: fmt.Sprintf("Install the %q CLI tool", n),
+			Run: func(_ *cobra.Command, _ []string) {
+				installWithGolang(v)
+			},
+		})
+	}
+
+	return cs
+}
+
 var installDopplerCmd = &cobra.Command{
 	Use:   "doppler",
 	Short: "Install the `doppler` CLI tool",
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(_ *cobra.Command, _ []string) {
 		installWithHomebrew("gnupg")
 		installWithHomebrew("dopplerhq/cli/doppler")
 	},
@@ -47,40 +70,8 @@ var installDopplerCmd = &cobra.Command{
 var installBufCmd = &cobra.Command{
 	Use:   "buf",
 	Short: "Install the `buf` CLI tool",
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(_ *cobra.Command, _ []string) {
 		installWithHomebrew("bufbuild/buf/buf")
-	},
-}
-
-var installMigrateCmd = &cobra.Command{
-	Use:   "golang-migrate",
-	Short: "Install the `migrate` CLI tool",
-	Run: func(cmd *cobra.Command, args []string) {
-		installWithGolang("github.com/golang-migrate/migrate/v4/cmd/migrate")
-	},
-}
-
-var installSQLcCmd = &cobra.Command{
-	Use:   "sqlc",
-	Short: "Install the `sqlc` CLI tool",
-	Run: func(cmd *cobra.Command, args []string) {
-		installWithGolang("github.com/sqlc-dev/sqlc/cmd/sqlc")
-	},
-}
-
-var installMockeryCmd = &cobra.Command{
-	Use:   "mockery",
-	Short: "Install the `mockery` CLI tool",
-	Run: func(cmd *cobra.Command, args []string) {
-		installWithGolang("github.com/vektra/mockery/v2")
-	},
-}
-
-var installIfacemakerCmd = &cobra.Command{
-	Use:   "ifacemaker",
-	Short: "Install the `ifacemaker` CLI tool",
-	Run: func(cmd *cobra.Command, args []string) {
-		installWithGolang("github.com/vburenin/ifacemaker")
 	},
 }
 
