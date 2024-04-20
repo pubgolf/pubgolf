35c15638282d0d6a0605bdf6119aae04cd52ec64
Eric Morris <git@thedeerchild.com>
2024-04-19 23:52:09 -0400

    chore(webapp): lint fixes

--- web-app/src/lib/helpers/formatters.ts
+++ web-app/src/lib/helpers/formatters.ts
@@ -1,5 +1,5 @@
-import type { Player } from "$lib/proto/api/v1/shared_pb";
-import { scoringCategoryToDisplayName } from "./scoring-category";
+import type { Player } from '$lib/proto/api/v1/shared_pb';
+import { scoringCategoryToDisplayName } from './scoring-category';
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, { timeStyle: 'medium' });
 export function formatTimestamp(d: Date) {
@@ -11,7 +11,7 @@ export function formatListAnd(l: string[]) {
 	return listFormatter.format(l);
 }
 export function formatPlayerName(player: Player, eventKey: string) {
-  const cat = player?.events.find((x) => x.eventKey == eventKey)?.scoringCategory;
-  const catName = cat ? scoringCategoryToDisplayName[cat] : 'None';
-  return `${player?.data?.name} (${catName})`;
+	const cat = player?.events.find((x) => x.eventKey == eventKey)?.scoringCategory;
+	const catName = cat ? scoringCategoryToDisplayName[cat] : 'None';
+	return `${player?.data?.name} (${catName})`;
 }
