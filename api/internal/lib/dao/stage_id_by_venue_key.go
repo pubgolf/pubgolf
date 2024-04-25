0a33a3fa838e91e68a40086d22fa694b4dfdb886
Eric Morris <git@thedeerchild.com>
2024-04-25 00:23:42 -0400

    add(dao/StageIDByVenueKey): cache

--- api/internal/lib/dao/stage_id_by_venue_key.go
+++ api/internal/lib/dao/stage_id_by_venue_key.go
@@ -7,11 +7,13 @@ import (
 	"github.com/pubgolf/pubgolf/api/internal/lib/models"
 )
 
+var stageIDByVenueKeyCache = makeCache[dbc.StageIDByVenueKeyParams, models.StageID](cacheSizeEvent, cacheExpirationDurable)
+
 // StageIDByVenueKey looks up the stage ID given the client-facing version of the identifier (the venueKey).
 func (q *Queries) StageIDByVenueKey(ctx context.Context, eventID models.EventID, venueKey models.VenueKey) (models.StageID, error) {
 	defer daoSpan(&ctx)()
 
-	return q.dbc.StageIDByVenueKey(ctx, dbc.StageIDByVenueKeyParams{ //nolint:wrapcheck // Trivial passthrough
+	return wrapWithCache(ctx, stageIDByVenueKeyCache, q.dbc.StageIDByVenueKey, dbc.StageIDByVenueKeyParams{
 		EventID:  eventID,
 		VenueKey: venueKey,
 	})
